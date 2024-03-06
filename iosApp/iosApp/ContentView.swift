7f58e53274c32695281889f345f2185c79494d47
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2024-03-06 11:53:54 +0900

    iOSで最後のロケット打ち上げ成功時刻を表示できるように

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,16 +2,34 @@ import SwiftUI
 import Shared
 
 struct ContentView: View {
-    let phrases = Greeting().greet()
+    @ObservedObject private(set) var viewModel: ViewModel
+    
     var body: some View {
-        List(phrases, id: \.self) {
-            Text($0)
+        ListView(phrases: viewModel.greetings).onAppear { self.viewModel.startObserving() }
+    }
+}
+
+extension ContentView {
+    @MainActor
+    class ViewModel: ObservableObject {
+        @Published var greetings: Array<String> = []
+
+        func startObserving() {
+            Task {
+                for await phrase in Greeting().greet() {
+                    self.greetings.append(phrase)
+                }
+            }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ListView: View {
+    let phrases: Array<String>
+
+    var body: some View {
+        List(phrases, id: \.self) {
+            Text($0)
+        }
     }
 }
