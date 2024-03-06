6be3716511f1a67f7cecc9cdc5149f290cad1d30
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2024-03-06 11:06:03 +0900

    commonMainにkotlinx.datetimeの依存関係追加

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
         }
     }
 }
