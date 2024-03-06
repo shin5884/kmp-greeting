bda151b6578340cef8de8c328d382fa841e8c46e
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2024-03-06 11:29:42 +0900

    sharedにcoroutine, ktorの依存関係追加

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 kotlin {
@@ -23,11 +24,23 @@ kotlin {
             isStatic = true
         }
     }
+
+    val ktorVersion = "2.3.7"
     
     sourceSets {
         commonMain.dependencies {
             // put your Multiplatform dependencies here
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
         }
     }
 }
