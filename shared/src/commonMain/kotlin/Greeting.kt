e788d78e409411d77d490a0f1f312429facd0f79
Shingo Kobayashi <shingo.kobayashi@sansan.com>
2024-03-06 10:42:39 +0900

    Greeting#greetが返す文字列を変更

--- shared/src/commonMain/kotlin/Greeting.kt
+++ shared/src/commonMain/kotlin/Greeting.kt
@@ -1,7 +1,11 @@
+import kotlin.random.Random
+
 class Greeting {
     private val platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+
+        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
     }
 }
